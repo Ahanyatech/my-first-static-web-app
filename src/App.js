b7e2c65523245325ad2c9038a7b052b16f76e608
Ahanyatech <109302366+Ahanyatech@users.noreply.github.com>
2023-03-01 16:42:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import axios from 'axios';
+
+const fetchQuotes = async () => {
+	try {
+		const res = await axios.get(
+			`https://famous-quotes4.p.rapidapi.com/random`,
+			{
+				headers: {
+					'x-rapidapi-host': 'famous-quotes4.p.rapidapi.com',
+					'x-rapidapi-key': API_KEY
+				},
+				params: {category: 'all', count: '10'}
+			}
+		);
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 function App() {
-  const value = 'React';
-  return <div><h1>Hello from my first application in {value} </h1></div>;
+  const value = async () => {
+	try {
+		const res = await axios.get(
+			`https://app-backend-api.azurewebsites.net/WeatherForecast`,
+			{
+				headers: {
+				},
+				params: {}
+			}
+		);
+	} catch (err) {
+		console.log(err);
+	}
+};
+  return <div><h1>Weather today is {value} </h1></div>;
 }
 
 export default App;
