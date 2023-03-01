abd9d136b1a45edebeaf8a1c9305d62e00a483f3
Ahanyatech <109302366+Ahanyatech@users.noreply.github.com>
2023-03-01 16:53:17 -0600

    Update App.js
    Remove Error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-const fetchQuotes = async () => {
-	try {
-		const res = await axios.get(
-			`https://famous-quotes4.p.rapidapi.com/random`,
-			{
-				headers: {
-					'x-rapidapi-host': 'famous-quotes4.p.rapidapi.com',
-					'x-rapidapi-key': API_KEY
-				},
-				params: {category: 'all', count: '10'}
-			}
-		);
-	} catch (err) {
-		console.log(err);
-	}
-};
-
 function App() {
   const value = async () => {
 	try {
