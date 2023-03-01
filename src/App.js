8a4af9add559f9922eac995e5e19ce42f481a878
Ahanyatech <109302366+Ahanyatech@users.noreply.github.com>
2023-03-01 13:27:07 -0600

    Update App.js
    Added Code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'React';
-  return <div>Hello from my first application in {value}</div>;
+  return <div><h1>Hello from my first application in {value} </h1></div>;
 }
 
 export default App;
