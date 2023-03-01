66911749ab3de8fbca4f6a0ecef5bc43105b5b69
Ahanyatech <109302366+Ahanyatech@users.noreply.github.com>
2023-03-01 17:32:54 -0600

    Update App.js
    Removed Error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 
-
-  const value = async () => {
+function App() {
 	try {
-		const res = await axios.get(
-			`https://app-backend-api.azurewebsites.net/WeatherForecast`,
-			{
-				headers: {},
-				params: {}
-			}		
-		);
-	return <div><h1>Weather today is {res} </h1></div>;
-	} catch (err) {
-		console.log(err);
+			const res = await axios.get(
+				`https://app-backend-api.azurewebsites.net/WeatherForecast`,
+				{
+					headers: {},
+					params: {}
+				}		
+			);
+		return <div><h1>Weather today is {res} </h1></div>;
+		} 
+	catch (err) {
+			console.log(err);
+		}
 	}
-};
   
 
 export default App;
