6d307cc10710773405d4e3ae940693d9855d8ade
Ahanyatech <109302366+Ahanyatech@users.noreply.github.com>
2023-03-01 17:26:42 -0600

    Update App.js
    Fixed Error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 
-function App() {
+
   const value = async () => {
 	try {
 		const res = await axios.get(
 			`https://app-backend-api.azurewebsites.net/WeatherForecast`,
 			{
-				headers: {
-				},
+				headers: {},
 				params: {}
 			}
+		return <div><h1>Weather today is {res} </h1></div>;
 		);
 	} catch (err) {
 		console.log(err);
 	}
 };
-  return <div><h1>Weather today is {value} </h1></div>;
-}
+  
 
 export default App;
